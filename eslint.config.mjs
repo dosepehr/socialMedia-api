990ec241c9b1181313038ffe23e3c770d070671a
dosepehr <dosepehr@gmail.com>
2025-01-07 12:02:16 +0330

    config eslint & prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            sourceType: 'commonjs',
+            env: {
+                node: true, // Enables Node.js environment
+            },
+        },
+        rules: {
+            'no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^next$|^res$|^req$|^err$',
+                },
+            ],
+        },
+    },
+    {
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+    pluginJs.configs.recommended,
+];
