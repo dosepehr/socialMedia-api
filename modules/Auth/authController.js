44dda25ead76aef769d2196874fa62514203dc78
dosepehr <dosepehr@gmail.com>
2025-01-07 16:20:46 +0330

    send email to users

--- modules/Auth/authController.js
+++ modules/Auth/authController.js
@@ -1,6 +1,7 @@
 const expressAsyncHandler = require('express-async-handler');
 const User = require('../User/userModel');
 const signToken = require('../../utils/funcs/signToken');
+const Email = require('../../utils/classes/Email');
 
 exports.signup = expressAsyncHandler(async (req, res, next) => {
     const JWT_EXPIRES = +process.env.JWT_EXPIRES.slice(0, 2);
@@ -14,6 +15,7 @@ exports.signup = expressAsyncHandler(async (req, res, next) => {
     // TODO validation
     const newUser = await User.create(userData);
     const token = signToken({ id: newUser._id });
+    await new Email({ email: newUser.email }, '').sendWelcome();
     res.cookie('auth', `Bearer ${token}`, {
         expires: new Date(Date.now() + JWT_EXPIRES * 24 * 60 * 60 * 1000),
         secure: req.secure, // if https was on
