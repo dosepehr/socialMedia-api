057375459202af5ea2c930120563c6c6b357de1c
dosepehr <dosepehr@gmail.com>
2025-01-08 15:01:39 +0330

    add follows data in get page controller

--- modules/Page/pageController.js
+++ modules/Page/pageController.js
@@ -1,8 +1,39 @@
-const { getOne } = require('../Factory/factoryController');
+const expressAsyncHandler = require('express-async-handler');
+const Follow = require('../Follow/followModel');
 const User = require('../User/userModel');
 
-exports.getPage = getOne(User, {}, [
-    {
-        path: 'posts',
-    },
-]);
+exports.getPage = expressAsyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+
+    // Use `Promise.all` to fetch user, posts, followers, and followings concurrently
+    const [user, followersData, followingsData] = await Promise.all([
+        User.findOne({ _id: id }).populate({ path: 'posts' }).lean(),
+        Follow.find({ following: id })
+            .populate('follower', 'username name _id')
+            .lean(),
+        Follow.find({ follower: id })
+            .populate('following', 'username name _id')
+            .lean(),
+    ]);
+
+    if (!user) {
+        return res.status(404).json({
+            status: false,
+            message: 'Resource not found',
+        });
+    }
+
+    // Process followers and followings
+    const followers = followersData.map((f) => f.follower);
+    const followings = followingsData.map((f) => f.following);
+
+    // Build the response object
+    res.status(200).json({
+        status: true,
+        data: {
+            ...user,
+            followers,
+            followings,
+        },
+    });
+});
